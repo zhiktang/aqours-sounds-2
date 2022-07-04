6258938103110caea3d6187afa4a12e43f239e03
zhiktang <zhikai.tang123@gmail.com>
2022-07-04 11:57:04 -0500

    add counter(functionality questionable)

--- script.js
+++ script.js
@@ -3,43 +3,43 @@ const sounds = ['Chika', 'Riko', 'You', 'Dia', 'Kanan', 'Mari', 'Hanamaru', 'Rub
 var count = [0 , 0, 0, 0, 0, 0, 0, 0, 0];
 var keyon = 'none';
 sounds.forEach((sound)=> {
-    
+    update();
     var buttondiv = document.createElement('div');
     buttondiv.className = 'buttondiv';
     var btn = document.createElement('img');
     btn.className = 'btn';
+    btn.title = sound;
     btn.src = './images/' + sound + '.png';
     buttondiv.appendChild(btn);
-
+    var description = document.createElement('button');
+    description.className = 'description';
+    update(description,sound);
+    buttondiv.appendChild(description);
     //add the button to the div with the id of sound
     document.getElementById('buttons').appendChild(buttondiv);
 
-    update();
+    
     btn.addEventListener('click', ()=> {
         //console.log(sound);
         console.log('sending click');
-        click(sound,btn);
+        click(sound,description);
         //console.log('requesting update 1');
         //update();
-        //btn.innerText = count[sounds.indexOf(sound)];
         const audio = new Audio(`sounds/${sound}.mp3`);
         audio.load();
         audio.play();
         //console.log('requesting update 2');
         //update();
-        //btn.innerText = count[sounds.indexOf(sound)];
     });    
     btn.addEventListener('mouseenter', ()=> {
         //console.log("enter");
         keyon = sound;
         update();
-        btn.innerText = count[sounds.indexOf(sound)];
 
     });
     btn.addEventListener('mouseleave', ()=> {
         update();
         keyon = 'none';
-        btn.innerText = sound;
     });
 
 });
@@ -64,7 +64,29 @@ function update() {
     //console.log(count);
     //console.log(count.length);
 };
-function click(person,btn) {
+function update(description,person) {
+    const xhttp = new XMLHttpRequest();
+    xhttp.open('GET', 'https://count.alhub.net', true);
+    xhttp.send();
+    xhttp.onreadystatechange = function() {
+       // console.log(xhttp.readyState);
+       // console.log("status: " + xhttp.status);
+        if (this.readyState == 4 && this.status == 200) {
+           // console.log("enter if");
+            //console.log(this.responseText);
+            const res = this.responseText;
+           // console.log(file);
+            //console.log(res);
+            lines = JSON.parse(res);
+            console.log(lines);
+            count = lines;
+            description.innerText = count[sounds.indexOf(person)];
+        }
+    }
+    //console.log(count);
+    //console.log(count.length);
+};
+function click(person,description) {
     //console.log(person);
     const xhttp = new XMLHttpRequest();
     xhttp.open('POST', 'https://count.alhub.net', true);
@@ -77,7 +99,7 @@ function click(person,btn) {
             if(keyon == person) {
                 singular = JSON.parse(this.responseText);
                 console.log('changed button text');
-                btn.innerText = singular;
+                description.innerText = singular;
             }
         }
         
